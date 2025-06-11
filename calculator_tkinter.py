ec34fdd03144c41e2ec0f8f8cc710c32e3d213e2
shiva-rsl <shrasouli2@gmail.com>
2025-06-11 15:29:10 +0330

    feat(ui): enhance appearance of the calculator

--- calculator_tkinter.py
+++ calculator_tkinter.py
@@ -5,33 +5,51 @@ window = tk.Tk()
 window.title('Scientific Calculator')
 window.geometry('500x700')
 window.resizable(width=False, height=False)
-window.configure(background="#818181")
+window.configure(background='#818181')
 
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=1)
+window.columnconfigure(2, weight=1)
 
 result_label = tk.Entry(
     master=window,
     font=('sans-serif', 20, 'bold'),
     border=5,
-    justify='right'
+    justify='right',
+    state='readonly',
 )
-result_label.grid(row=0, column=1, padx=43, pady=15)
+result_label.grid(row=0, column=0, columnspan=6, pady=(30, 10),)
 
-buttons = [
-    {'text': ''}, {'text': ''}, {'text': ''}, {'text': ''},
-    {'text': ''}, {'text': ''}, {'text': ''}, {'text': ''},
-    {'text': ''}, {'text': ''}, {'text': ''}, {'text': ''},
-    {'text': ''}, {'text': ''}, {'text': ''}, {'text': ''},
-    {'text': ''}, {'text': ''}, {'text': ''}, {'text': ''},
-    {'text': ''}, {'text': ''}, {'text': ''}, {'text': ''},
-    {'text': ''}, {'text': '7', 'bg': 'black',}, {'text': '8', 'bg': 'black',},
-    {'text': '9', 'bg': 'black',}, {'text': '%'}, {'text': '^'}, {'text': ''},
-    {'text': '4', 'bg': 'black',}, {'text': '5', 'bg': 'black',}, 
-    {'text': '6', 'bg': 'black',}, {'text': '*'}, {'text': '/'},
-    {'text': 'AC', 'bg': 'red',}, {'text': '1', 'bg': 'black',}, 
-    {'text': '2', 'bg': 'black',}, {'text': '3', 'bg': 'black',}, 
-    {'text': '+', 'height': 6}, {'text': '-'}, {'text': 'CE', 'bg': 'red',}, 
-    {'text': '+/-', 'bg': 'black',}, {'text': '0', 'bg': 'black',}, 
-    {'text': '.', 'bg': 'black',}, {'text': '='},
+
+buttons_features = [
+    {'text': 'sin', 'bg': '#060706',}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'},
+    {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'},
+    {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'},
+    {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'},
+    {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '(', 'bg': '#060706'}, {'text': ')', 'bg': '#060706'},
+    {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'}, {'text': '99', 'bg': '#060706'},
+    {'text': '99', 'bg': '#060706'}, {'text': '7'}, {'text': '8'}, {'text': '9'},
+    {'text': '%', 'bg':'#858282'}, {'text': '^', 'bg':'#858282'},
+    {'text': '99', 'bg': '#060706'}, {'text': '4'}, {'text': '5'}, {'text': '6'}, 
+    {'text': '*', 'bg':'#858282'}, {'text': '/', 'bg':'#858282'}, 
+    {'text': 'AC', 'bg': '#DC1212'}, {'text': '1'}, {'text': '2'}, {'text': '3'}, 
+    {'text': '+', 'bg':'#858282'}, {'text': '-', 'bg':'#858282'}, 
+    {'text': 'CE', 'bg': '#DC1212'}, {'text': '+/-'}, {'text': '0'}, {'text': '.'}, 
+    {'text': '=', 'bg':'#858282'},
 ]
+
+
+calc_buttons = []
+plus_button = None
+equal_button = None
+skip_indexes = []
+
+
+for key_object in buttons_features:
+    bg = key_object.get('bg')
+    fg = 
+    
+
+
 
 window.mainloop()
